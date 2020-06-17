f4e748b6f7bb0162c0ea3895afff4b404d1b80b4
AngelLuna0902 <67038429+AngelLuna0902@users.noreply.github.com>
2020-06-17 02:04:19 -0500

    LoadFont
    Script para cargar fuentes en RStudio, se usa la librería extrafont. Para graficar se usa ggplot y la data soil que se utiliza es de librería agricolae

--- LoadFonts.R
+++ LoadFonts.R
@@ -1,17 +1,20 @@
 library(pacman)
 p_load(tidyverse, agricolae, extrafont)
 insta
-#♀font_import()#se importa por única vez
-loadfonts(device="win") #se carga cuando se trabaje con el cambio de fuentes
+#font_import()#se importa por única vez
+loadfonts(device="win") #se carga cuando se vaya a trabajar en una nueva sesión al
+# similar a cargar una librería, device = "win" es para cagrar las fuentes de windows
 fonts()#vector con todas las fuentes del sistema
 fonttable()#carga las fuentes como tabla
 
-data("soil")
-windowsFonts(A = windowsFont("Times New Roman")) 
+data("soil")#data de libría agricolae
+windowsFonts(A = windowsFont("Times New Roman")) # sirve para renombrar una fuente por
+# un nombre más corto, es opcional 
 p <- ggplot()+
   geom_point(data=soil, mapping = aes(x=pH, y=EC))+
   labs(x="pH", y="Conductividad eléctrica", title = "Propiedades del suelo")+
   theme_bw()+
   theme(text=element_text(family="A", face = "bold", size = 12),
         plot.title = element_text(hjust = 0.5))
-ggsave(plot=p, 'GRAFICOS/tutorialfont.png', width = 15, height = 7, dpi = 300)
+ggsave(plot=p, 'GRAFICOS/tutorialfont.png', width = 15, height = 7, dpi = 300)#la
+# dirección se debe cambiar por la ruta interna del equipo 
